bab267d52bb6fac9c5969424b35670af3690e5ce
SanaAitLahcen <162061965+SanaAitLahcen@users.noreply.github.com>
2025-01-05 15:36:41 +0100

    Update ArbreB.h

--- ArbreBinaire/ArbreB.h
+++ ArbreBinaire/ArbreB.h
@@ -7,23 +7,39 @@
 #include <iomanip>
 using namespace std;
 
+// Définition de la classe ArbreB qui représente un arbre binaire
 class ArbreB
 {
 private:
-	int value;
-	ArbreB* Fdt;
-	ArbreB* Fgh;
+    int value;             // Valeur du noeud courant
+    ArbreB* Fdt;          // Pointeur vers le fils droit
+    ArbreB* Fgh;          // Pointeur vers le fils gauche
 
 public:
-	ArbreB(int );
-	ArbreB* getFdt();
-	ArbreB* getFgh();
-	int getVal();
-	void InsertArbB(int val);
-	bool SupprimerNoeud(int val);
-	ArbreB* RechercheDsArbre(int);
-	void ParcourBFS();
-	ArbreB* RechercheDsArbreBFS(int val);
+    // Constructeur pour initialiser un noeud avec une valeur donnée
+    ArbreB(int val);
 
+    // Getter pour le fils droit (Fdt)
+    ArbreB* getFdt();
+
+    // Getter pour le fils gauche (Fgh)
+    ArbreB* getFgh();
+
+    // Getter pour la valeur du noeud
+    int getVal();
+
+    // Insérer une nouvelle valeur dans l'arbre binaire
+    void InsertArbB(int val);
+
+    // Supprimer un noeud avec une valeur spécifique de l'arbre
+    bool SupprimerNoeud(int val);
+
+    // Recherche un noeud avec une valeur spécifique dans l'arbre de manière récursive
+    ArbreB* RechercheDsArbre(int val);
+
+    // Parcours en largeur (BFS) pour visiter tous les noeuds de l'arbre
+    void ParcourBFS();
+
+    // Recherche un noeud avec une valeur spécifique dans l'arbre en utilisant BFS
+    ArbreB* RechercheDsArbreBFS(int val);
 };
-
